d4cf9a2aea2757a8b0f9102a0723782f4ebcb9cc
Alexandru Busuioc <alex96bus@gmail.com>
2018-11-23 19:39:39 +0200

    [11/23/2018] Added string interpolation

--- SmartATM2/Account.cs
+++ SmartATM2/Account.cs
@@ -1,30 +1,24 @@
-﻿//remove unused usings
-using System;
+﻿using System;
 
 namespace SmartATM2
 {
-    //To do: why is this class non-static, but the methods static?
     internal class Account
     {
         private decimal balance = 0;
-        //To do: Delete unnecessary whitespaces + indent code (hotkey is CTRL+K, D) 
+ 
         internal void AddBalance()
         {
             balance += Convert.ToDecimal(Console.ReadLine());
         }
 
-        //this method can either not return a value, and you set the result in the class
-        //or you can create an instance of this class and remove static properties/methods
-        //also rename this method to : Withdraw
-        //for currencies always use : decimal (read on msdn)
         internal void Withdraw()
         {
             int withdraw = int.Parse(Console.ReadLine());
+
             if (withdraw > balance)
             {
                 Console.WriteLine("Insufficient balance!");
             }
-            //why does the withdraw and balance have to be multiplies of 100 if values are decimal?
             else if (withdraw % 10 != 0)
             {
                 Console.WriteLine("Please enter the amount in multiples of 10");
@@ -33,9 +27,6 @@ namespace SmartATM2
             {
                 balance -= withdraw;
             }
-            //To do: If withdraw is greater than balance you will always return a negative number
-            //Suggestion: remove unneccesary write lines from here, it isn't the responsability of the account
-            //to tell the user his balance
         }
 
         internal decimal GetBalance()
--- SmartATM2/Program.cs
+++ SmartATM2/Program.cs
@@ -1,12 +1,9 @@
-﻿//Remove unused usingss
-using System;
+﻿using System;
 
 namespace SmartATM2
 {
-    //add access modifier to class
     internal class Program
     {
-        //add access modifier to method
         internal static void Main(string[] args)
         {
             Account account = new Account();
@@ -16,30 +13,26 @@ namespace SmartATM2
                 Console.WriteLine("2. Deposit balance");
                 Console.WriteLine("3. Withdraw balance");
                 Console.WriteLine("4. Change PIN");
-                //You can create here a clear screen method (look into System.Console methods)
-                //Also, exit function is not implemented (look into System methods)
                 Console.WriteLine("5. Clear");
                 Console.WriteLine("6. Exit");
-                //Why not use TryParse?
+
                 int menu;
                 int.TryParse(Console.ReadLine(), out menu);
-                // Why dont you create an instance of this account outside the menu? Before while
+
                 switch (menu)
                 {
-                    //Why don't you separate variables from writeline statement
-                    //Read about string interpolation
                     case 1:
                         Console.WriteLine($"Your current balance is {account.GetBalance()} ");
                         break;
                     case 2:
                         Console.WriteLine("How much would you like to deposit?");
                         account.AddBalance();
-                        Console.WriteLine("Your balance is " + account.GetBalance());
+                        Console.WriteLine($"Your balance is {account.GetBalance()}");
                         break;
                     case 3:
                         Console.WriteLine("How much would you like to withdraw?");
                         account.Withdraw();
-                        Console.WriteLine("Your balance is " + account.GetBalance());
+                        Console.WriteLine($"Your balance is {account.GetBalance()}");
                         break;
                     case 4:
                         break;
