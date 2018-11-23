0a6837c3a945acc44f5d71678856193dc55ff402
Alexandru Grosu <alex_grosu13@yahoo.ro>
2018-11-23 19:33:13 +0200

    Added all

--- SmartATM2/Account.cs
+++ SmartATM2/Account.cs
@@ -1,53 +1,46 @@
 ﻿//remove unused usings
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SmartATM2
 {
     //To do: why is this class non-static, but the methods static?
-    public class Account
+    internal class Account
     {
-       public static double DefaultBalance = 0;
-       //To do: Delete unnecessary whitespaces + indent code (hotkey is CTRL+K, D) 
-
-        public static double AddBalance()
+        private decimal balance = 0;
+        //To do: Delete unnecessary whitespaces + indent code (hotkey is CTRL+K, D) 
+        internal void AddBalance()
         {
-            double newBalance = DefaultBalance + Convert.ToDouble(Console.ReadLine());
-            return newBalance;
+            balance += Convert.ToDecimal(Console.ReadLine());
         }
 
         //this method can either not return a value, and you set the result in the class
         //or you can create an instance of this class and remove static properties/methods
         //also rename this method to : Withdraw
         //for currencies always use : decimal (read on msdn)
-        public static double WithDraw()
-        {          
+        internal void Withdraw()
+        {
             int withdraw = int.Parse(Console.ReadLine());
-            double result = DefaultBalance - withdraw;
-            if (withdraw > DefaultBalance)
+            if (withdraw > balance)
             {
                 Console.WriteLine("Insufficient balance!");
             }
-            //why does the withdraw and balance have to be multiplies of 100 if values are double?
-            else if(withdraw % 100 != 0)
+            //why does the withdraw and balance have to be multiplies of 100 if values are decimal?
+            else if (withdraw % 10 != 0)
             {
-                Console.WriteLine("Please enter the amount in multiples of 100");
+                Console.WriteLine("Please enter the amount in multiples of 10");
             }
             else
             {
-                result = DefaultBalance - withdraw;
-                Console.WriteLine("Your new balance is " + result);
+                balance -= withdraw;
             }
             //To do: If withdraw is greater than balance you will always return a negative number
             //Suggestion: remove unneccesary write lines from here, it isn't the responsability of the account
             //to tell the user his balance
-            return result;
         }
 
-        
+        internal decimal GetBalance()
+        {
+            return balance;
+        }
     }
 }
--- SmartATM2/Program.cs
+++ SmartATM2/Program.cs
@@ -1,59 +1,58 @@
 ﻿//Remove unused usingss
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SmartATM2
 {
     //add access modifier to class
-    class Program
+    internal class Program
     {
         //add access modifier to method
-        static void Main(string[] args)
+        internal static void Main(string[] args)
         {
-            Console.WriteLine("1. Check balance");
-            Console.WriteLine("2. Deposit balance");
-            Console.WriteLine("3. Withdraw balance");
-            Console.WriteLine("4. Change PIN");
-            //You can create here a clear screen method (look into System.Console methods)
-            //Also, exit function is not implemented (look into System methods)
-            Console.WriteLine("5. Exit");
-
-            //Why not use TryParse?
-            int menu = int.Parse(Console.ReadLine());
-            MainMenu(menu);
-        }
-
-        public static void MainMenu(int menu)
-        {
-            // Why dont you create an instance of this account outside the menu? Before while
-            switch (menu)
+            Account account = new Account();
+            while (true)
             {
-                //Why don't you separate variables from writeline statement
-                //Read about string interpolation
-                case 1:
-                    Console.WriteLine("Your current balance is " + Account.DefaultBalance);
-                    break;
-                case 2:
-                    Console.WriteLine("How much would you like to deposit?");
-                    Console.WriteLine("Your new balance is " + Account.AddBalance());
-                    break;
-                case 3:
-                    Console.WriteLine("How much would you like to withdraw?");
-                    Console.WriteLine("Your new balance is ", Account.WithDraw());
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                default:
-                    //You can be more explicit here, you can tell the user he entered a wrong number
-                    //And show him what to enter
-                    Console.WriteLine("Something went wrong, try again!");
-                    break;
+                Console.WriteLine("1. Check balance");
+                Console.WriteLine("2. Deposit balance");
+                Console.WriteLine("3. Withdraw balance");
+                Console.WriteLine("4. Change PIN");
+                //You can create here a clear screen method (look into System.Console methods)
+                //Also, exit function is not implemented (look into System methods)
+                Console.WriteLine("5. Clear");
+                Console.WriteLine("6. Exit");
+                //Why not use TryParse?
+                int menu;
+                int.TryParse(Console.ReadLine(), out menu);
+                // Why dont you create an instance of this account outside the menu? Before while
+                switch (menu)
+                {
+                    //Why don't you separate variables from writeline statement
+                    //Read about string interpolation
+                    case 1:
+                        Console.WriteLine($"Your current balance is {account.GetBalance()} ");
+                        break;
+                    case 2:
+                        Console.WriteLine("How much would you like to deposit?");
+                        account.AddBalance();
+                        Console.WriteLine("Your balance is " + account.GetBalance());
+                        break;
+                    case 3:
+                        Console.WriteLine("How much would you like to withdraw?");
+                        account.Withdraw();
+                        Console.WriteLine("Your balance is " + account.GetBalance());
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        Console.Clear();
+                        break;
+                    case 6:
+                        return;
+                    default:
+                        Console.WriteLine("You entered a wrong selection!");
+                        break;
+                }
             }
-        }
+        } 
     }
 }
