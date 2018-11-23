f4f26bec587c41f36ce45ffb142cb1daa78da1a1
Alexandru Grosu <alex_grosu13@yahoo.ro>
2018-11-23 18:26:15 +0200

    Added ATM project

---
+++ SmartATM2/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartATM2
+{
+    public class Account
+    {
+       public static double DefaultBalance = 0;
+        
+
+        public static double AddBalance()
+        {
+            double newBalance = DefaultBalance + Convert.ToDouble(Console.ReadLine());
+            return newBalance;
+        }
+
+        public static double WithDraw()
+        {          
+            int withdraw = int.Parse(Console.ReadLine());
+            double result = DefaultBalance - withdraw;
+            if (withdraw > DefaultBalance)
+            {
+                Console.WriteLine("Insufficient balance!");
+            }
+            else if(withdraw % 100 != 0)
+            {
+                Console.WriteLine("Please enter the amount in multiples of 100");
+            }
+            else
+            {
+                result = DefaultBalance - withdraw;
+                Console.WriteLine("Your new balance is " + result);
+            }
+            return result;
+        }
+
+        
+    }
+}
---
+++ SmartATM2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartATM2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Check balance");
+            Console.WriteLine("2. Deposit balance");
+            Console.WriteLine("3. Withdraw balance");
+            Console.WriteLine("4. Change PIN");
+            Console.WriteLine("5. Exit");
+
+            int menu = int.Parse(Console.ReadLine());
+            MainMenu(menu);
+        }
+
+        public static void MainMenu(int menu)
+        {
+            switch (menu)
+            {
+                case 1:
+                    Console.WriteLine("Your current balance is " + Account.DefaultBalance);
+                    break;
+                case 2:
+                    Console.WriteLine("How much would you like to deposit?");
+                    Console.WriteLine("Your new balance is " + Account.AddBalance());
+                    break;
+                case 3:
+                    Console.WriteLine("How much would you like to withdraw?");
+                    Console.WriteLine("Your new balance is ", Account.WithDraw());
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    Console.WriteLine("Something went wrong, try again!");
+                    break;
+            }
+        }
+    }
+}
